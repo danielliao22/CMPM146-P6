c9410623979d8ad3fee18fb3f31019619bacf40c
dpwalton <dpwalton@ucsc.edu>
2020-11-11 22:22:57 -0800

    dumping all my stuff into the git

---
+++ src/test.py
@@ -0,0 +1,33 @@
+import copy
+import heapq
+import metrics
+import multiprocessing.pool as mpool
+import os
+import random
+import shutil
+import time
+import math
+from ga import *
+
+width = 200
+height = 16
+
+if __name__ == "__main__":
+    Individual = Individual_Grid
+
+    random.seed(1)
+    ind = Individual.random_individual()
+    print("\nIndividual:")
+    for i in ind.genome:
+        print(listToString(i))
+
+    g = ind.genome
+    for row in range(height):
+        for col in range(width):
+            if g[row][col] == 'T':
+                print("Location of pipe:", row, col)
+
+    ind.mutate(ind.genome)
+    print("\nMutated:")
+    for i in ind.genome:
+        print(listToString(i))
