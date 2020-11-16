f1b5f6de103aef571d58aee2d0c3cfe3b0973ba9
dpwalton <dpwalton@ucsc.edu>
2020-11-15 17:03:35 -0800

    reworked random_ind for DE encoding

--- src/test.py
+++ src/test.py
@@ -13,21 +13,45 @@ width = 200
 height = 16
 
 if __name__ == "__main__":
-    Individual = Individual_Grid
+    # Individual = Individual_Grid
 
-    random.seed(1)
+
+# grid stuff
+    # Individual = Individual_Grid
+    # random.seed(1)
+    # ind = Individual.random_individual()
+    # print("\nIndividual:")
+    # for i in ind.genome:
+    #     print(listToString(i))
+    #
+    # g = ind.genome
+    # for row in range(height):
+    #     for col in range(width):
+    #         if g[row][col] == 'T':
+    #             print("Location of pipe:", row, col)
+    #
+    # ind.mutate(ind.genome)
+    # print("\nMutated:")
+    # for i in ind.genome:
+    #     print(listToString(i))
+
+    Individual = Individual_DE
+    random.seed(7)
     ind = Individual.random_individual()
     print("\nIndividual:")
-    for i in ind.genome:
+    # for i in ind.genome:
+    #     print(i)
+    lev = ind.to_level()
+    for i in lev:
         print(listToString(i))
 
-    g = ind.genome
-    for row in range(height):
-        for col in range(width):
-            if g[row][col] == 'T':
-                print("Location of pipe:", row, col)
-
-    ind.mutate(ind.genome)
-    print("\nMutated:")
-    for i in ind.genome:
+    mut = Individual_DE(ind.mutate(ind.genome))
+    print("\nmutated:")
+    lev = mut.to_level()
+    for i in lev:
         print(listToString(i))
+
+    # print("stairs of mut:")
+    # print(mut.get_stairs())
+    # print("pipes of mut:")
+    # print(mut.get_pipes())
